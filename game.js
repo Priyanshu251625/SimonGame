c5066f9a9551c697b161e236a7fb8b7afea2e8d9
Priyanshu251625 <priyanshu.rajhans25@gmail.com>
2025-05-13 13:08:54 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,69 @@
+var buttonColors=["red", "blue", "green", "yellow"];
+var gamePattern=[];
+var userClickedPattern=[];
+var level=0;
+function newSequence(){
+    level++;
+    $("#level-title").text("Level "+level);
+    var randomNumber=Math.floor(Math.random()*4);
+    var randomChoosenColor=buttonColors[randomNumber];
+    gamePattern.push(randomChoosenColor);
+    $("#"+randomChoosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChoosenColor);
+}
+$(".btn").click(function(){
+    var userChoosenColor=$(this).attr("id");
+    userClickedPattern.push(userChoosenColor);
+    //console.log(userClickedPattern);
+    playSound(userChoosenColor);
+    animatePress(userChoosenColor);
+    checkAnswer(userClickedPattern.length-1);
+});
+function checkAnswer(index){
+    if(gamePattern[index]===userClickedPattern[index]){
+        console.log("success");
+        if(gamePattern.length===userClickedPattern.length){
+            userClickedPattern=[];
+            setTimeout(function(){
+                newSequence();
+            },1000);
+        }
+        
+    }
+    else{
+        playSound("wrong");
+        console.log("wrong");
+        $("body").addClass("game-over");
+        setTimeout(function(){
+        $("body").removeClass("game-over");
+        },200)
+        $("h1").text("Game Over, Press Any Key to Restart");
+        startOver();
+    }
+    
+
+}
+function playSound(name){
+    var audio=new Audio("./sounds/"+name+".mp3");
+    audio.play();
+}
+function animatePress(name){
+    $("#"+name).addClass("pressed");
+    setTimeout(function(){
+        $("#"+name).removeClass("pressed")
+    },100)
+}
+var started=false;
+$(document).keydown(function(event){
+    if(started===false){
+        started=true;
+        newSequence();
+    }
+})
+function startOver(){
+    level=0;
+    gamePattern=[];
+    started=false;
+}
+
+
